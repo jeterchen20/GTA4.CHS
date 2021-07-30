e546b2fd1dc11422e5f95d34088600ccb646537f
ClansChen <clanschen@gmail.com>
2021-07-31 00:22:49 +0800

    2021/07/30备份

--- WHMTool/WHMTool.cpp
+++ WHMTool/WHMTool.cpp
@@ -3,12 +3,43 @@
 #include <QDropEvent>
 #include <QMimeData>
 #include <QtZlib/zlib.h>
+#include "pgBase.h"
 
-struct RscHeader {
-    quint32   dwMagic;
-    quint32   dwVersion;
-    quint32   dwFlags;
-};
+QByteArray inflate_array(QByteArray& compressed, uint uncompressed_size)
+{
+    QByteArray uncompressed;
+    uncompressed.resize(uncompressed_size);
+    ulong dest_size = uncompressed_size;
+
+    //估计是从header算出SystemMemorySize+GraphicsMemorySize作为解压后数据大小
+    uncompress(reinterpret_cast<unsigned char*>(uncompressed.data()), &dest_size, reinterpret_cast<unsigned char*>(compressed.data()), compressed.size());
+
+    return uncompressed;
+}
+
+void uncompress_rsc(const QString& filename)
+{
+    QFile file;
+
+    file.setFileName(filename);
+    if (!file.open(QIODevice::ReadOnly))
+        return;
+
+    RSCHeader header;
+    file.read(reinterpret_cast<char*>(&header), sizeof(RSCHeader));
+
+    uint uncompressed_size = ((header.flags & 0x7FF) << (((header.flags >> 11) & 0xF) + 8)) +
+        (((header.flags >> 15) & 0x7FF) << (((header.flags >> 26) & 0xF) + 8));
+
+    QByteArray compressed = file.readAll();
+    QByteArray uncompressed = inflate_array(compressed, uncompressed_size);
+
+    file.setFileName(filename + ".uncompressed");
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate))
+        return;
+
+    file.write(uncompressed);
+}
 
 WHMTool::WHMTool(QWidget* parent)
     : QMainWindow(parent)
--- WHMTool/pgBase.h
+++ WHMTool/pgBase.h
@@ -1,15 +1,36 @@
 #pragma once
 
+typedef unsigned short ushort;
 typedef unsigned int uint;
 typedef unsigned char uchar;
 
-struct pgPtr {
-    int offset : 28;
-    int type : 4;
-    //if (type == 5) {   
-    //    local uint doffset = FTell();
-    //    FSeek(offset);
-    //    byte DataStart;
-    //    FSeek(doffset);
-    //}
+//SparkIV
+enum ResourceType :uint
+{
+    TextureXBOX = 0x7, // xtd
+    ModelXBOX = 0x6D, // xdr
+    Generic = 0x01, // xhm / xad (Generic files as rsc?)
+    Bounds = 0x20, // xbd, wbd
+    Particles = 0x24, // xpfl
+    Particles2 = 0x1B, // xpfl
+
+    Texture = 0x8, // wtd
+    Model = 0x6E, // wdr
+    ModelFrag = 0x70, //wft
 };
+
+enum CompressionType :ushort
+{
+    LZX = 0xF505,
+    Deflate = 0xDA78
+};
+
+struct RSCHeader
+{
+    uint magic;
+    ResourceType rtype;
+    uint flags;
+    CompressionType ctype;
+};
+
+//SparkIV.ResourceFile.cs
